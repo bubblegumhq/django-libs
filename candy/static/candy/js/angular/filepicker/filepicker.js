786e88fe68471db1d4fccd9beb86298b26303f61
Asim Hussain <jawache@gmail.com>
2015-02-12 15:10:26 +0000

    Added pickSingle to FilePicker which returns a promise

--- candy/static/candy/js/angular/filepicker/filepicker.js
+++ candy/static/candy/js/angular/filepicker/filepicker.js
@@ -6,6 +6,39 @@ mod.service('FilePicker', ['$q',
 		function ($q) {
 			var FilePickerService = {};
 
+			FilePickerService.pickSingle = function () {
+				var deferred = $q.defer();
+				filepicker.pickAndStore(
+					{
+						mimetype: "image/*",
+						folders: false
+					},
+					{
+						location: "S3"
+					},
+					function (Blobs) {
+						//NOTE: We only support loading one file at a time!
+						if (Blobs.length > 0) {
+							var blob = Blobs[0];
+							//NOTE: Grab with and height from filepicker
+							filepicker.stat(
+								blob,
+								{
+									width: true,
+									height: true
+								},
+								function (metadata) {
+									blob.width = metadata.width;
+									blob.height = metadata.height;
+									deferred.resolve(blob);
+								}
+							)
+						}
+					}
+				);
+				return deferred.promise();
+			};
+
 			FilePickerService.pick = function (callback) {
 				filepicker.pickAndStore(
 					{
