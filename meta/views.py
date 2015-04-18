8afe8008b74bf4540f11bf3dd89cf8513d752b18
Asim Hussain <jawache@gmail.com>
2015-04-18 15:12:44 +0100

    Meta data fixes

--- meta/views.py
+++ meta/views.py
@@ -2,8 +2,21 @@ from __future__ import unicode_literals
 
 from django.core.exceptions import ImproperlyConfigured
 
-from . import settings
+# from . import settings
+from django.conf import settings
 
+SITE_PROTOCOL = getattr(settings, 'META_SITE_PROTOCOL', None)
+SITE_DOMAIN = getattr(settings, 'META_SITE_DOMAIN', None)
+SITE_TYPE = getattr(settings, 'META_SITE_TYPE', None)
+SITE_NAME = getattr(settings, 'META_SITE_NAME', None)
+INCLUDE_KEYWORDS = getattr(settings, 'META_INCLUDE_KEYWORDS', [])
+DEFAULT_KEYWORDS = getattr(settings, 'META_DEFAULT_KEYWORDS', [])
+IMAGE_URL = getattr(settings, 'META_IMAGE_URL', settings.STATIC_URL)
+USE_OG_PROPERTIES = getattr(settings, 'META_USE_OG_PROPERTIES', False)
+USE_TWITTER_PROPERTIES = getattr(settings, 'META_USE_TWITTER_PROPERTIES', False)
+USE_GOOGLEPLUS_PROPERTIES = getattr(settings, 'META_USE_GOOGLEPLUS_PROPERTIES', False)
+USE_SITES = getattr(settings, 'META_USE_SITES', False)
+DEFAULTS = getattr(settings, 'META_DEFAULTS', {})
 
 class Meta(object):
     """ Helper for building context meta object """
@@ -13,34 +26,34 @@ class Meta(object):
     _image = None
 
     def __init__(self, **kwargs):
-        self.use_sites = kwargs.get('use_sites', settings.USE_SITES)
-        self.title = kwargs.get('title')  or settings.DEFAULTS.get("title")
-        self.description = kwargs.get('description')  or settings.DEFAULTS.get("description")
+        self.use_sites = kwargs.get('use_sites', USE_SITES)
+        self.title = kwargs.get('title')  or DEFAULTS.get("title")
+        self.description = kwargs.get('description')  or DEFAULTS.get("description")
         self.extra_props = kwargs.get('extra_props')
         self.extra_custom_props = kwargs.get('extra_custom_props')
         self.keywords = kwargs.get('keywords')
-        self.url = kwargs.get('url')  or settings.DEFAULTS.get("url")
-        self.image = kwargs.get('image')  or settings.DEFAULTS.get("image")
-        self.object_type = kwargs.get('object_type', settings.SITE_TYPE)
-        self.site_name = kwargs.get('site_name', settings.SITE_NAME)
+        self.url = kwargs.get('url')  or DEFAULTS.get("url")
+        self.image = kwargs.get('image')  or DEFAULTS.get("image")
+        self.object_type = kwargs.get('object_type', SITE_TYPE)
+        self.site_name = kwargs.get('site_name', SITE_NAME)
         self.twitter_site = kwargs.get('twitter_site')
         self.locale = kwargs.get('locale')
-        self.use_og = kwargs.get('use_og', settings.USE_OG_PROPERTIES)
-        self.use_twitter = kwargs.get('use_twitter', settings.USE_TWITTER_PROPERTIES)
-        self.use_googleplus = kwargs.get('use_googleplus', settings.USE_GOOGLEPLUS_PROPERTIES)
+        self.use_og = kwargs.get('use_og', USE_OG_PROPERTIES)
+        self.use_twitter = kwargs.get('use_twitter', USE_TWITTER_PROPERTIES)
+        self.use_googleplus = kwargs.get('use_googleplus', USE_GOOGLEPLUS_PROPERTIES)
 
     def get_domain(self):
         if self.use_sites:
             from django.contrib.sites.models import Site
             return Site.objects.get_current().domain
-        if not settings.SITE_DOMAIN:
+        if not SITE_DOMAIN:
             raise ImproperlyConfigured('META_SITE_DOMAIN is not set')
-        return settings.SITE_DOMAIN
+        return SITE_DOMAIN
 
     def get_protocol(self):
-        if not settings.SITE_PROTOCOL:
+        if not SITE_PROTOCOL:
             raise ImproperlyConfigured('META_SITE_PROTOCOL is not set')
-        return settings.SITE_PROTOCOL
+        return SITE_PROTOCOL
 
     def get_full_url(self, url):
         if not url:
@@ -66,14 +79,14 @@ class Meta(object):
     @keywords.setter
     def keywords(self, keywords):
         if keywords is None:
-            kws = settings.DEFAULT_KEYWORDS
+            kws = DEFAULT_KEYWORDS
         else:
             if not hasattr(keywords, '__iter__'):
                 # Not iterable
                 raise ValueError('Keywords must be an intrable')
             kws = [k for k in keywords]
-            if settings.INCLUDE_KEYWORDS:
-                kws += settings.INCLUDE_KEYWORDS
+            if INCLUDE_KEYWORDS:
+                kws += INCLUDE_KEYWORDS
         seen = set()
         seen_add = seen.add
         self._keywords = [k for k in kws if k not in seen and not seen_add(k)]
@@ -96,7 +109,7 @@ class Meta(object):
             self._image = None
             return
         if not image.startswith('http') and not image.startswith('/'):
-            image = '%s%s' % (settings.IMAGE_URL, image)
+            image = '%s%s' % (IMAGE_URL, image)
         self._image = self.get_full_url(image)
 
 
@@ -115,17 +128,17 @@ class MetadataMixin(object):
     site_name = None
     twitter_site = None
     locale = None
-    use_sites = settings.USE_SITES
-    use_og = settings.USE_OG_PROPERTIES
+    use_sites = USE_SITES
+    use_og = USE_OG_PROPERTIES
 
     def get_meta_class(self):
         return self.meta_class
 
     def get_protocol(self):
-        return settings.SITE_PROTOCOL
+        return SITE_PROTOCOL
 
     def get_domain(self):
-        return settings.SITE_DOMAIN
+        return SITE_DOMAIN
 
     def get_meta_title(self, context={}):
         return self.title
@@ -143,10 +156,10 @@ class MetadataMixin(object):
         return self.image
 
     def get_meta_object_type(self, context={}):
-        return self.object_type or settings.SITE_TYPE
+        return self.object_type or SITE_TYPE
 
     def get_meta_site_name(self, context={}):
-        return self.site_name or settings.SITE_NAME
+        return self.site_name or SITE_NAME
 
     def get_meta_extra_props(self, context={}):
         return self.extra_props
